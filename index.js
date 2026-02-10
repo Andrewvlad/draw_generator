88a06d755b6fd67a82db513a691741138d507492
Andrewvlad <andrewv54@gmail.com>
2026-02-10 12:16:26 -0500

    Rewrite logic for readability

--- index.js
+++ index.js
@@ -14,29 +14,27 @@ const main = (
     dives = 10,
     minPoints = 3,
 ) => {
-    const ret = [[]];
+    const ret = [];
 
     let pool = [...randoms, ...blocks];
-    let curPoints = 0;
 
-    while (true) {
-        if (!pool.length) { // Regenerate empty pool
-            pool = [...randoms, ...blocks];
+    while (ret.length < dives) {
+        ret.unshift([]); // Add empty dive-flow to start
+        let curPoints = 0;
+
+        while (curPoints < minPoints) {
+            if (!pool.length) { // Regenerate empty pool
+                pool = [...randoms, ...blocks];
+            }
+
+            const [randomPoint] = randomItem(pool);
+
+            curPoints = curPoints + (Number.isInteger(randomPoint) ? 2 : 1); // 1 point for randoms, 2 for blocks
+            ret[0].push(randomPoint); // Add to latest dive-flow
         }
-
-        const [randomPoint] = randomItem(pool);
-
-        curPoints = curPoints + (Number.isInteger(randomPoint) ? 2 : 1); // 1 point for randoms, 2 for blocks
-        ret[0].push(randomPoint); // Add to latest dive-flow
-
-        if (curPoints >= minPoints) {
-            if (ret.length >= dives) return ret;
-
-            curPoints = 0; // Reset points
-            ret.unshift([]); // Add empty dive-flow to start
-        }
-
     }
+
+    return ret;
 };
 
 console.log(main());
